5410bec3d5b25da34fd12d4426a4a9a05fedaa20
aaronkthai <104555268+aaronkthai@users.noreply.github.com>
2024-05-03 23:55:25 -0700

    Add files via upload

--- ZotineraryScript.js
+++ ZotineraryScript.js
@@ -10,3 +10,19 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open');
+}
+
+function openPopup(type) {
+    var popupId = 'popup-' + type;
+    var popup = document.getElementById(popupId);
+    if (popup) {
+        popup.style.display = 'block';
+    }
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
