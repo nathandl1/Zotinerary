c191215b8bba580fc11f28539727203f0524498a
AlwaysOscar <oscarrrodriguez2020@gmail.com>
2024-05-03 22:09:27 -0700

    popup draft

--- home.js
+++ home.js
@@ -1,16 +1,41 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('open');
 }
 
-function openPopup(type) {
-    var popupId = 'popup-' + type;
-    var popup = document.getElementById(popupId);
-    if (popup) {
-        popup.style.display = 'block';
-    }
-}
-
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+  
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+  
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal')
+      closeModal(modal)
+    })
+  })
+  
+  function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
