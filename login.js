7a217216d735f6a54af708b3df6a5a1c44929548
AlwaysOscar <oscarrrodriguez2020@gmail.com>
2024-05-03 20:43:31 -0700

    Login and Sign up Page updated
    default username password
    
    change only handles new updated username and password on local storage (2 variable)

--- login.js
+++ login.js
@@ -5,12 +5,14 @@ document.getElementById('login-form').addEventListener('submit', function (event
     var password = document.getElementById('password').value;
     var errorMessage = document.getElementById('error-message');
 
-    // Example of basic validation
-    if (username === 'username' && password === 'password') {
-        // Successful login
-        window.location.href = 'home.html'; // Redirect to home page
+    // Retrieve the stored username and password from localStorage
+    var storedUsername = localStorage.getItem('username');
+    var storedPassword = localStorage.getItem('password');
+
+    if (username === storedUsername && password === storedPassword) {
+        // Redirect to home page
+        window.location.href = 'home.html';
     } else {
-        // Display error message
         errorMessage.textContent = 'Invalid username or password. Please try again.';
     }
 });
