587846470b946426def5df2266ae512c498ae136
nathandl1 <104229699+nathandl1@users.noreply.github.com>
2024-05-16 18:09:56 -0700

    Nathan

--- app/ui/fonts.ts
+++
@@ -1,3 +0,0 @@
-import {Crimson_Pro} from 'next/font/google';
- 
-export const crimson = Crimson_Pro({ subsets: ['latin'] });
---
+++ zotinerary/app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import {Crimson_Pro} from 'next/font/google';
+ 
+export const crimson = Crimson_Pro({ subsets: ['latin'] });
